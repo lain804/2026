01909738ce1fb2f5a79ea869ce0c644abf3f0325
Vez <103569111+Vezise@users.noreply.github.com>
2026-04-23 23:09:21 +0100

    Update Violet.lua

--- Vez/Violet/Violet.lua
+++ Vez/Violet/Violet.lua
@@ -771,14 +771,16 @@ GUI.contain.bottom.contain["Play Sound"].BackgroundTransparency = 0
 GUI.contain.bottom.contain["Play Sound"].BackgroundColor3 = Color3.fromHex("#962EFF")
 GUI.little.contain.ViewportFrame.BackgroundColor3 = Color3.fromRGB(0,0,0)
 
-do
-	if getgenv().getcustomasset ~= nil then return end
-	local Notification = Library:createBigButtonNoti("WARNING!", "Your exploit does not support 'getcustomasset', this UI may be detected in some games.", Logger:GetAsset("Warning"), 10)
+task.spawn(function()
+	do
+		if getgenv().getcustomasset ~= nil then return end
+		local Notification = Library:createBigButtonNoti("WARNING!", "Your exploit does not support 'getcustomasset', this UI may be detected in some games.", Logger:GetAsset("Warning"), 10)
 
-	Notification:createButton("OK", function()
-		Notification:Close()
-	end)
-end
+		Notification:createButton("OK", function()
+			Notification:Close()
+		end)
+	end
+end)
 
 do
 	Handle(function()
